0a50292690564dad69b4f770d5bfaa887e13eeb5
Marc_LUPI <marc.lupi@protonmail.com>
2025-09-29 15:17:38 +0200

    Ajout icônes multi-dimensions, optimisation PWA, corrections diverses

--- service_worker.js
+++ service_worker.js
@@ -3,8 +3,14 @@ const OFFLINE_URL = "/offline.html";
 
 const FILES_TO_CACHE = [
   OFFLINE_URL,
+  "/offline.html",
+  "/manifest.json",
   "/ASSET/offline_styles.css",
   "/IMG/fond.avif",
+  "/IMG/icon_192.png",
+  "/IMG/icon_256.png",
+  "/IMG/icon_384.png",
+  "/IMG/icon_512.png",
   "/IMG/logo.png"
 ];
 
@@ -39,10 +45,10 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     fetch(event.request).catch(() => {
       if (event.request.mode === "navigate") {
-        // ✅ Si navigation et offline → page custom
+        // Si navigation et offline → page custom
         return caches.match(OFFLINE_URL);
       }
-      // ✅ Sinon → on renvoie ce qu’il y a dans le cache
+      // Sinon → on renvoie ce qu’il y a dans le cache
       return caches.match(event.request);
     })
   );
